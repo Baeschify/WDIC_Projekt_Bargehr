5047a630902963825a582476ce9e9d55570a2fd2
Baeschify <71693023+Baeschify@users.noreply.github.com>
2021-03-09 09:53:33 +0100

    commit to 'main'

--- RPi_Programm.py
+++ RPi_Programm.py
@@ -21,9 +21,8 @@ def callbackbkw():
 	print(picture)
 	img = PhotoImage(file = picture)
 	Label(pictureFrame, image=img).grid(row=0, column=0, padx=10, pady=3)
-	#MainWindow.after(0, uart)
 	MainWindow.mainloop()
-	#MainWindow.update()
+	
 
 def callbackfwd():
 	global images
@@ -35,9 +34,8 @@ def callbackfwd():
 	print(picture)
 	img = PhotoImage(file = picture)
 	Label(pictureFrame, image=img).grid(row=0, column=0, padx=10, pady=3)
-	#MainWindow.after(0, uart)
 	MainWindow.mainloop()
-	#MainWindow.update()
+	
 
 #vorhandene Ordner/Files anzeigen:
 def list_folder():
@@ -45,12 +43,31 @@ def list_folder():
         print(file)
         images.append(file)
 
+#Uart Routine
 def uart(main_window):
 	print("Hello")
 	print("World")
 	while True:
-		main_window.after(0, callbackfwd)
-		sleep(1)
+        print("Hello")
+        xneu = ser.readline()
+	    # der Empfangene Befehl (ein Bild weiter oder zurück) steht jetzt in xneu
+        if(xneu == 'fwd'):	#Hier kommt quasi der selbe Befehlsblock wie wenn man im GUI auf den forward Button drücken würde
+            main_window.after(0, callbackfwd)
+            #imagenumber=imagenumber + 1 
+            #picture = 'images_png/' + images[imagenumber]
+            #print(picture)
+            #img = PhotoImage(file = picture)
+            #Label(pictureFrame, image=img).grid(row=0, column=0, padx=10, pady=3)
+        elif(xneu == 'bkw'):
+            main_window.agter(0, callbackbkw)
+            #imagenumber=imagenumber - 1 
+            #picture = 'images_png/' + images[imagenumber]
+            #print(picture)
+            #img = PhotoImage(file = picture)
+            #Label(pictureFrame, image=img).grid(row=0, column=0, padx=10, pady=3)
+        print("World")
+		#main_window.after(0, callbackfwd)
+		#sleep(1)
 
 list_folder()
 print(images)
@@ -67,9 +84,9 @@ buttonFrame.grid(row=1, column=0, padx=0, pady=0) # Relative Position und Seiten
 pictureFrame = Frame(MainWindow, width=1500, height = 400) 
 pictureFrame.grid(row=0, column=0, padx=0, pady=0)
 
-#picture = 'images/Kekssortenliste_Seite_1.png'
-#img = MainWindow.PhotoImage(file = picture)
-#Label(pictureFrame, image=img).grid(row=0, column=0, padx=10, pady=3)
+picture = 'images_png/DS1Z_QuickPrint1.png'
+img = PhotoImage(file = picture)
+Label(pictureFrame, image=img).grid(row=0, column=0, padx=10, pady=3)
 
 buttonClose = Button(buttonFrame, bg='#FF0000', text='X', command=MainWindow.destroy, width=50)
 buttonClose.grid(row=0, column=1, padx=0, pady=0)
@@ -86,13 +103,34 @@ buttonbkw.grid(row=0, column=0, padx=0, pady=0)
 #Eine Lösung im Internet ist in einer While-Schleife, wo sich ja auch die UART-Empfangsroutine befinden wird,
 #'tk.update_idletasks()' und 'tk.update()' zu verwende, wobei dies nicht ganz funktioniert.
 
-#MainWindow.after(0, uart)
-#MainWindow.mainloop()
+
+
+#Serielle Schnittstelle initialisieren:
+ser = serial.Serial(
+        port='/dev/ttyS0', 		#Replace ttyS0 with ttyAM0 for Pi1,Pi2,Pi0
+        baudrate = 9600,
+        parity=serial.PARITY_NONE,
+        stopbits=serial.STOPBITS_ONE,
+        bytesize=serial.EIGHTBITS
+        #timeout=1				#weiß leider nicht ganz was timeout genau macht
+)
+# Davor muss noch in den Einstellungen des RPi's die serielle Schnittstelle aktiviert werden:
+# https://www.electronicwings.com/raspberry-pi/raspberry-pi-uart-communication-using-python-and-c
+# in diesem Tutorial ist alles beschrieben.
+
 
 thread = Thread(target=uart, args=(MainWindow,), daemon=True)
 thread.start()
 
 MainWindow.mainloop()
 
+
+
 #Tkinter kann leider keine jpg Bilder anzeigen, sondern nur png und andere, was derzeit noch zu Problemen mit dem Anzeigen der
 #Bilder mit sich bringt
+
+
+#In der Konsole zuvor eingeben, damit Programmtechnische Funktionen installiert werden
+#'sudo apt-get install python python-tk idle python-pmw python-pil --yes'
+#'sudo apt-get install minicom python-serial'
+#'sudo apt-get install xrdp
